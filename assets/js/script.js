86a449a380f5a1a5c4486ba931528e184b3d4165
Oliver Jones <ogj21@cam.ac.uk>
2020-09-19 21:01:22 +0100

    Fixed so that swup will correctly load gallery from different page and console errors do not appear.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 const swup = new Swup()
+
+
+function init(){
+	if (document.querySelector("#pig")){
+		var imageData = [
+			{filename: 'apples', aspectRatio: 1.5},
+			{filename: 'kiwi', aspectRatio: 1.5},
+			{filename: 'bananas', aspectRatio: 1.5},
+		];
+
+		var options = {
+			urlForSize: function(filename, size) {
+					return 'generated/assets/images/' + filename + '-' + size + '.jpg';
+				},
+			// ...
+		};
+		var pig = new Pig(imageData, options).enable();
+	}
+}
+
+init();
+swup.on('contentReplaced', init);
