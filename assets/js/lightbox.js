d5c630b2c399aa2ec0a17ba0657b89fc83bf5f49
Oliver Jones <ogj21@cam.ac.uk>
2020-11-06 23:03:23 +0000

    initial site build

---
+++ assets/js/lightbox.js
@@ -0,0 +1,68 @@
+/*
+ * - Add an event listener for document click
+ * - Define a function that filters the unwanted click events on the document
+ */
+
+// Add an event listener for document click
+document.addEventListener('click', lightboxClick);
+
+// Define a function that filters the unwanted click events on the document
+function lightboxClick(event) {
+	var elem = event.target,
+		elemID = elem.getAttribute('id'),
+		lightboxImg = document.getElementById('lightbox-image'),
+		lightbox = document.getElementById("lightbox-overlay"),
+		newImg = new Image();
+		fp = '';
+		extension = '';
+
+	// If we click any of these 2 elements, close the lightbox
+	if (elemID == 'lightbox-image' || elemID == 'lightbox-overlay' || elem.tagName == "NAV") {
+		event.preventDefault();
+
+		lightboxImg.src = "";	// stops old image flashing up when selecting new one
+		lightbox.classList.remove('visible');
+		lightboxImg.style.display = "none";
+		lightbox.getElementsByTagName("NAV")[0].style.display = "none";
+	}
+	// If we click an element with the attribute "data-lightbox", show the lightbox
+	else if (elem.hasAttribute('data-lightbox')) {
+		event.preventDefault();
+
+		newImg.onload = function() {
+			lightboxImg.src = this.src;
+		}
+
+		newImg.src = elem.getAttribute('data-lightbox');
+		lightbox.classList.add('visible');
+		lightboxImg.style.display = "block";
+	}
+
+	else if (elem.tagName == "IMG") {
+		newImg.onload = function() {
+			lightboxImg.src = this.src;
+		}
+
+		fp = elem.getAttribute('src')
+		extension = fp.split('.').slice(-1)[0];
+		fp = fp.replace(/(-[0-9]+-[0-9a-f]{9})?\.(je?pg|png|gif)/, '');
+		console.log(fp);
+
+		if (fp.includes('generated/')){
+			fp = fp.replace('generated/', '');
+		}
+
+		newImg.src = fp + '.' + extension;
+		lightbox.classList.add('visible');
+		lightbox.style.display = "flex";
+		lightboxImg.style.display = "block";
+
+	}
+
+	else if (elemID == "mobile") {
+		lightbox.classList.add('visible');
+		lightbox.getElementsByTagName("NAV")[0].style.display = "block";
+		lightbox.style.display = "block";
+	}
+
+}
---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+const swup = new Swup()
+
+function getGalleryData() {
+	// return promise of JSON data
+	return $.getJSON("assets/js/pictures.json")
+}
+
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function init(){
+	topFunction();
+	if (document.querySelector("#pig")){
+		var imageData;
+		var pig;
+		var options = {
+			urlForSize: function(filename, size, hash) {
+					return 'generated/assets/images/' + filename + '-' + size + '-' + hash + '.jpg';
+				},
+
+			sizes: [ 20,  100,  250,  400,  600,  800,  1000, ].sort(function(a,b) {return a-b;}),
+
+			scroller: document.getElementsByClassName("main_window")[0]
+		};
+
+		getGalleryData().then( function(response){
+			imageData = response;
+			pig = new Pig(imageData, options).enable();
+			pig.enable();
+		});
+	}
+
+	var new_background = 'url("/assets/images/splash.jpg")';
+	var backgrounds = document.getElementsByClassName("side_bar_img");
+	if (document.querySelector("aside")){
+		var aside_element = document.querySelector("aside");
+		new_background = 'url("' + aside_element.id + '")';
+	}
+	if (document.querySelector(".img_on").style.backgroundImage != new_background){
+		for (i=0; i<2; i++){
+			if (backgrounds[i].className.includes("img_on")){
+				backgrounds[i].className = backgrounds[i].className.replace(" img_on", "")
+			}
+			else {
+				backgrounds[i].style.backgroundImage = new_background;
+				backgrounds[i].className += " img_on";
+			}
+		}
+	}
+}
+
+init();
+swup.on('contentReplaced', init);
