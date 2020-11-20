d1fc15be6d40f0113df12e3c562e02f8143d9e42
Oliver Jones <ogj21@cam.ac.uk>
2020-11-20 23:43:29 +0000

    Close button added

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -6,6 +6,7 @@
 // configure carousel.
 var flkty
 var flkty_set = false;
+var lightbox_visible = false;
 
 // Add an event listener for document click
 document.addEventListener('click', lightboxClick);
@@ -16,25 +17,26 @@ function lightboxClick(event) {
 		elemID = elem.getAttribute('id'),
 		lightboxCar = document.getElementById('lightbox-carousel'),
 		lightbox = document.getElementById("lightbox-overlay"),
-		newImg = new Image();
-		fp = '';
-		extension = '';
+		closeBtn = document.querySelector('.close')
 
 	// If we click any of these 2 elements, close the lightbox
-	if (elemID == 'lightbox-overlay' || elem.tagName == "NAV" || elem.tagName == "A") {
-		event.preventDefault();
+	if (lightbox_visible){
+		if (elemID == 'lightbox-overlay' || elem.tagName == "NAV" || elem.tagName == "A" || elem.className == "close" || elem.tagName == "path" || elem.tagName == "svg") {
+			event.preventDefault();
 
-		// lightboxImg.src = "/assets/images/spinner.svg";	// stops old image flashing up when selecting new one
-		lightbox.classList.remove('visible');
-		if (flkty_set){
-			flkty.destroy()
-			lightboxCar.innerHTML = '';
-			flkty_set = false;
+			// lightboxImg.src = "/assets/images/spinner.svg";	// stops old image flashing up when selecting new one
+			lightbox.classList.remove('visible');
+			if (flkty_set){
+				flkty.destroy()
+				lightboxCar.innerHTML = '';
+				flkty_set = false;
+			}
+			lightboxCar.style.display = "none";
+			lightbox.getElementsByTagName("NAV")[0].style.display = "none";
+			closeBtn.style.display = "none";
+			lightbox_visible = false;
 		}
-		lightboxCar.style.display = "none";
-		lightbox.getElementsByTagName("NAV")[0].style.display = "none";
 	}
-
 	else if ((elem.tagName == "IMG") && !(lightbox.classList.contains('visible'))) {
 		pageImgs = document.querySelector("main").getElementsByTagName("img");
 		lightboxCar.style.display = "initial";
@@ -64,11 +66,11 @@ function lightboxClick(event) {
 				i = j;
 			}
 			if(!pageImgs[j].srcset){
-				var fp = pageImgs[j].getAttribute('src')
-				extension = fp.split('.').slice(-1)[0];
+				var fp = pageImgs[j].getAttribute('src');
+				var extension = fp.split('.').slice(-1)[0];
 				fp = fp.replace(/(-[0-9]+-[0-9a-f]{9})?\.(je?pg|png|gif)/, '');
 				fp = fp.replace('generated/', '');
-				pageImgs[j].src = fp + '.jpg'
+				pageImgs[j].src = fp + '.jpg';
 			}
 		}
 
@@ -99,6 +101,8 @@ function lightboxClick(event) {
 		lightbox.classList.add('visible');
 		lightbox.style.alignItems = "center";
 		lightbox.style.display = "flex";
+		closeBtn.style.display = "block";
+		lightbox_visible = true;
 	}
 
 	else if (elemID == "mobile") {
@@ -106,6 +110,8 @@ function lightboxClick(event) {
 		lightbox.style.alignItems = "initial";
 		lightbox.getElementsByTagName("NAV")[0].style.display = "flex";
 		lightbox.style.display = "flex";
+		closeBtn.style.display = "block";
+		lightbox_visible = true;
 	}
 
 }
